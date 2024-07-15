50767d69a2b95c6d8088fd806e9f055310652a98
limits220284 <2056901285@qq.com>
2024-07-16 01:11:21 +0800

    add query by file

--- handlers/api/query_handler.go
+++ handlers/api/query_handler.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
+	"recrem/config/etcd"
 	"recrem/forms"
 	"recrem/gpt/openai"
 	"recrem/models"
 
 	"github.com/gin-gonic/gin"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 type QueryHandler struct {
@@ -23,7 +27,7 @@ func (q *QueryHandler) QueryByQuestion(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// userID := queryInfoForm.UserID
+	userID := queryInfoForm.UserID
 	question := queryInfoForm.Question
 	log.Println("Received UserID:", queryInfoForm.UserID)
 	log.Println("Received Question:", queryInfoForm.Question)
@@ -52,7 +56,60 @@ func (q *QueryHandler) QueryByQuestion(ctx *gin.Context) {
 		return
 	}
 	fmt.Printf("Retrieved embedding response: %+v\n", embeddingResp.Data[0].Object)
-	// 3. 相似度校验
 
-	// 4. 返回结果
+	// 通过userid获取所有的文件向量
+	userFilesEmbedding, err := etcd.EtcdIns.Get(context.Background(), userID, clientv3.WithPrefix())
+	if err != nil {
+		fmt.Println("Error getting all user files embedding:", err)
+		return
+	}
+
+	for _, kv := range userFilesEmbedding.Kvs {
+		fmt.Printf("the key is %s\n", kv.Key)
+	}
+	// 计算和问题最相近的向量
+	embeddingArrs := make([][]float64, 0)
+	for _, kv := range userFilesEmbedding.Kvs {
+		embeddingResp := models.EmbeddingResponse{}
+		err = json.Unmarshal(kv.Value, &embeddingResp)
+		if err != nil {
+			fmt.Println("Error unmarshalling response:", err)
+			return
+		}
+		embeddingArrs = append(embeddingArrs, embeddingResp.Data[0].Embedding)
+	}
+
+	mostSimilarIndex, maxSimilarity := findMostSimilarVector(embeddingResp.Data[0].Embedding, embeddingArrs)
+	fmt.Printf("Most similar vector index: %d, similarity: %f\n", mostSimilarIndex, maxSimilarity)
+}
+
+func cosineSimilarity(a, b []float64) float64 {
+	if len(a) != len(b) {
+		panic("Vectors must be the same length")
+	}
+	var dotProduct, magnitudeA, magnitudeB float64
+	for i := 0; i < len(a); i++ {
+		dotProduct += a[i] * b[i]
+		magnitudeA += a[i] * a[i]
+		magnitudeB += b[i] * b[i]
+	}
+	magnitudeA = math.Sqrt(magnitudeA)
+	magnitudeB = math.Sqrt(magnitudeB)
+	if magnitudeA == 0 || magnitudeB == 0 {
+		return 0
+	}
+	return dotProduct / (magnitudeA * magnitudeB)
+}
+
+func findMostSimilarVector(questionVector []float64, embeddingArrs [][]float64) (int, float64) {
+	var maxSimilarity float64
+	var mostSimilarIndex int
+	for i, vector := range embeddingArrs {
+		similarity := cosineSimilarity(questionVector, vector)
+		if similarity > maxSimilarity || i == 0 {
+			maxSimilarity = similarity
+			mostSimilarIndex = i
+		}
+	}
+	return mostSimilarIndex, maxSimilarity
 }
